67f9a57cc5de621e8785af8e7ec5226f19fa23f5
ackemannen <acke7101@gmail.com>
2025-06-04 01:57:03 +0200

    03: Home layout

---
+++ Documents/Webbsites/.FullStack/multi-ecommerce/src/app/(home)/footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+  return (
+    <footer className="flex border-t justify-between font-medium p-6">
+      <div>
+        <p>MultiShop</p>
+      </div>
+    </footer>
+  );
+};
