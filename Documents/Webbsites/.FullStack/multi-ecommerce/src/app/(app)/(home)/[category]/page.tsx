2cfb1b24958a748bdccd5b65c41993cffbb34e3b
ackemannen <acke7101@gmail.com>
2025-06-21 20:59:43 +0200

    15: multi tenancy

--- Documents/Webbsites/.FullStack/multi-ecommerce/src/app/(app)/(home)/[category]/page.tsx
+++ Documents/Webbsites/.FullStack/multi-ecommerce/src/app/(app)/(home)/[category]/page.tsx
@@ -4,6 +4,7 @@ import { getQueryClient, trpc } from "@/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { loadProductFilters } from "@/modules/products/search-params";
 import { ProductListView } from "@/modules/products/ui/views/product-list-view";
+import { DEFAULT_LIMIT } from "@/constatnts";
 
 interface Props {
   params: Promise<{
@@ -17,10 +18,11 @@ const Page = async ({ params, searchParams }: Props) => {
   const filters = await loadProductFilters(searchParams);
 
   const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(
-    trpc.products.getMany.queryOptions({
-      category,
+  void queryClient.prefetchInfiniteQuery(
+    trpc.products.getMany.infiniteQueryOptions({
       ...filters,
+      category,
+      limit: DEFAULT_LIMIT,
     })
   );
 
--- Documents/Webbsites/.FullStack/multi-ecommerce/src/modules/home/ui/components/search-filters/category-dropdown.tsx
+++ Documents/Webbsites/.FullStack/multi-ecommerce/src/modules/home/ui/components/search-filters/category-dropdown.tsx
@@ -3,7 +3,6 @@
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { useRef, useState } from "react";
-import { useDropdownPosition } from "./use-dropdown-position";
 import { SubcategoryMenu } from "./subcategory-menu";
 import Link from "next/link";
 import { CategoriesGetManyOutput } from "@/modules/categories/server/types";
@@ -21,7 +20,6 @@ export const CategoryDropdown = ({
 }: Props) => {
   const [isOpen, setIsOpen] = useState(false);
   const dropdownRef = useRef<HTMLDivElement | null>(null);
-  const { getDropdownPosition } = useDropdownPosition(dropdownRef);
 
   const onMouseEnter = () => {
     if (category.subcategories) {
@@ -32,8 +30,6 @@ export const CategoryDropdown = ({
   const onMouseLeave = () => {
     setIsOpen(false);
   };
-
-  const dropdownPosition = getDropdownPosition();
 
   return (
     <div
@@ -66,11 +62,7 @@ export const CategoryDropdown = ({
         )}
       </div>
 
-      <SubcategoryMenu
-        category={category}
-        isOpen={isOpen}
-        position={dropdownPosition}
-      />
+      <SubcategoryMenu category={category} isOpen={isOpen} />
     </div>
   );
 };
